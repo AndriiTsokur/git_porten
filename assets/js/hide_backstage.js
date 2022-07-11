17e63df4d1be127d5318efa59b14b4ec97a1d0f9
Andrii Tsokur <tsokur@gmail.com>
2022-07-12 00:31:33 +0300

    Minor improvements of mobile nav
    Now page doesn't scroll to the beginning after clicking the burger-menu.

--- assets/js/hide_backstage.js
+++ assets/js/hide_backstage.js
@@ -1,12 +1,13 @@
-var backstage = document.getElementById('myBackstage');
-var bodyScroll = document.getElementById('scroll');
-var trigger = document.getElementById('damn');
+let backstage = document.getElementById('myBackstage');
+let bodyScroll = document.getElementsByTagName('body')[0];
+let trigger = document.getElementById('damn');
+
 trigger.onclick = function() {
-	if (backstage.style.display === "block") {
-		backstage.style.display = "none";
-		bodyScroll.style.position = "static";
+	if (backstage.style.display === 'block') {
+		backstage.style.display = 'none';
+		bodyScroll.classList.remove('noscroll');
 	} else {
-		backstage.style.display = "block";
-		bodyScroll.style.position = "fixed";
+		backstage.style.display = 'block';
+		bodyScroll.classList.add('noscroll');
 	}
 }
